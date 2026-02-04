a60cb0cd44b4f03cbbc3e5da75544a13974a3fb3
Draiphis <val08350@gmail.com>
2026-02-04 11:59:28 +0100

    completed ex7

--- 1_Basics/exercices/07_affichage_potions.js
+++ 1_Basics/exercices/07_affichage_potions.js
@@ -2,3 +2,9 @@
 
 // Variable de l'exercice précédent (modifie selon tes valeurs)
 const potions = ["Potion de soin", "Potion de mana", "Potion d'endurance"];
+
+console.log(potions[0])
+console.log(potions[2])
+for(i in potions){
+    console.log(potions[i])
+}
