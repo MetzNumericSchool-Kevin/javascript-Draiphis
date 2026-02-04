8f7e378a69cbd6b96e076f7a37fb0d588368f778
Draiphis <val08350@gmail.com>
2026-02-04 08:48:48 +0100

    completed ex 1

--- 1_Basics/exercices/01_definition_variables.js
+++ 1_Basics/exercices/01_definition_variables.js
@@ -1,1 +1,10 @@
 // Définition de variables
+
+const nom_boutique = "Olive And Her"
+let stock_potion_soin = 6
+
+let prix_potion_soin = 2
+
+let is_open = true
+
+console.log(nom_boutique+stock_potion_soin+prix_potion_soin+is_open)
