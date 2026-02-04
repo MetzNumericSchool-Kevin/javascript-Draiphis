9fd8aae7c9499f063bed86b114c837b2b4a5b8ed
Draiphis <val08350@gmail.com>
2026-02-04 08:52:53 +0100

    finished ex2

--- 1_Basics/exercices/02_affichage_conditionnel.js
+++ 1_Basics/exercices/02_affichage_conditionnel.js
@@ -2,4 +2,11 @@
 
 // Variables de l'exercice précédent (modifie selon tes valeurs)
 const nom_boutique = "La Potion Enchantée";
-const boutique_ouverte = true;
+const boutique_ouverte = false;
+
+if (boutique_ouverte) {
+    console.log("Bienvenue dans la boutique " + nom_boutique+ " Aventurier !")
+}
+else {
+    console.log("La boutique " +nom_boutique+ " est fermée, revenez plus tard Aventurier ! 😴")
+}
